a94a60f51e03068386e31c42b513fa984c9be429
Megan Dotto <megan.dotto@gmail.com>
2018-06-29 04:57:39 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,8 +1,14 @@
-class Actor < ActiveRecord::Base[4.2]
-  def change
-    create_change :actors do |t|
-      t.string :first_name
-      t.string :last_name
+class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
     end
   end
 end
